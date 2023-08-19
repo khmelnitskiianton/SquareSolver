9ebeac7cb7fc614586a126154951897f7dc4c8fe
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-19 22:46:07 +0300

    Добавлена: функция чистки ввода, функция защиты от дурака, функция решения квадратки

--- Solver.cpp
+++ Solver.cpp
@@ -5,68 +5,41 @@
 
 int compare(double x, double y);
 
+double data(void);
+
+void clean(void);
+
+void solver(double *solutions,double a, double b, double c);
+
 int main(void)
 {
     //1 initialization all parametrs
 
-    double a = 0;
-    double b = 0;
-    double c = 0;
-    double x1 = 0;
-    double x2 = 0;
-    double d = 0;
+    double x = NAN;
+    double y = NAN;
+    double z = NAN;
 
     //2 Enter all data
 
-    printf("Enter quadratic equation: ax^2 + bx + c = 0. Parametrs is floating point numbers, like 1.0\n");
-    printf("a = ");
-    scanf("%lf",&a);
-    printf("b = ");
-    scanf("%lf",&b);
-    printf("c = ");
-    scanf("%lf",&c);
-    printf("(%.4lf)*x^2 + (%.4lf)*x + (%.4lf) = 0\n", a,b,c);
+    printf("Enter parametrs(numbers) of quadratic equation: ax^2 + bx + c = 0.\n");
+    x = data();
+    y = data();
+    z = data();
 
     //3 Enumeration
 
-    if(compare(a,0))
-    {
-        if(compare(b,0))
-        {
-            if(compare(c,0))
-            {
-                printf("Infinity of solutions. a = b = c = 0");
-            }
-            else
-            {
-                printf("No solutions. %.4lf = 0",c);
-            }
-        }
-        else
-        {
-            x1 = -(c/b);
-            printf("One solution. x = %.4lf", x1);
-        }
-    }
-    else
-    {
-        d = b*b-4*a*c;
-        if(d > 0)
-        {
-            x1 = (-b + sqrt(d))/(2*a);
-            x2 = (-b - sqrt(d))/(2*a);
-            printf("Discriminant is more then 0.Two solutions:\nx1 = %.4lf\nx2 = %.4lf\n",x1,x2);
-        }
-        else if(compare(d,0))
-        {
-            x1 = -b/(2*a);
-            printf("Discriminant equal to 0.One solution:\nx = %.4lf\n", x1);
-        }
-        else
-        {
-            printf("Discriminant is less then 0.No solutions.\n");
-        }
-    }
+    //array of solutions, first - count, second and third - numbers
+    double solutions[3] = {NAN,NAN,NAN};
+
+    solver(solutions,x,y,z);
+
+    int counter = (int)solutions[0];
+
+    if(counter == -1) printf("Infinity of solutions. a = b = c = 0");
+    if(counter == 0) printf("No solutions.");
+    if(counter == 1) printf("One solution. x = %lf", solutions[1]);
+    if(counter == 2) printf("Two solutions.\nx1 = %lf\nx2 = %lf",solutions[1],solutions[1]);
+
     return 0;
 }
 
@@ -77,4 +50,81 @@ int compare(double x, double y)
         return 1;
     else
         return 0;
+}
+
+//function to clear rubbish
+void clean(void)
+{
+    int ch = 0;//its char, but i use it only for input/output
+    while((ch = getchar()) != '\n')
+        putchar(ch);
+}
+
+//function of correct enter one double parametr
+double data(void)
+{
+    double h = NAN;
+    while((scanf("%lf",&h) != 1))
+    {
+        clean();
+        printf("\nWtf? Its not number. Another attemp you piece of shit!\n");
+    }
+    return h;
+}
+
+void solver(double *solutions, double a, double b, double c)
+{
+    double x1 = NAN;
+    double x2 = NAN;
+    double d = NAN;
+
+    if(compare(a,0))
+    {
+        if(compare(b,0))
+        {
+            if(compare(c,0))
+            {
+                //printf("Infinity of solutions. a = b = c = 0");
+                solutions[0] = -1;
+            }
+            else
+            {
+                //printf("No solutions. %lf = 0",c);
+                solutions[0] = 0;
+            }
+        }
+        else
+        {
+            x1 = -(c/b);
+            //printf("One solution. x = %lf", x1);
+            solutions[0] = 1;
+            solutions[1] = x1;
+        }
+    }
+    else
+    {
+        d = b*b-4*a*c;
+        if(d > 0)
+        {
+            x1 = (-b + sqrt(d))/(2*a);
+            x2 = (-b - sqrt(d))/(2*a);
+            //printf("Discriminant is more then 0.Two solutions:\nx1 = %lf\nx2 = %lf\n",x1,x2);
+            solutions[0] = 2;
+            solutions[1] = x1;
+            solutions[2] = x2;
+        }
+        else if(compare(d,0))
+        {
+            x1 = -b/(2*a);
+            //printf("Discriminant equal to 0.One solution:\nx = %lf\n", x1);
+            solutions[0] = 1;
+            solutions[1] = x1;
+        }
+        else
+        {
+            //printf("Discriminant is less then 0.No solutions.\n");
+            solutions[0] = 0;
+        }
+    }
+
 }
