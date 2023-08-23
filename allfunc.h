91966cfe9101f80935c1408647fc357ecbf8133e
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-23 18:01:02 +0300

    Общая полная своя библиотека

--- allfunc.h
+++ allfunc.h
@@ -1,3 +1,7 @@
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+
 //--------------------------------------------------------------------------------------
 //constants with define and enum
 
@@ -5,9 +9,9 @@
 #define QUANTITY 3          //quantity of parametrs (3 in quadratic equation)
 
 enum solutions{
-    INF_SOL = -1,
+    INF_SOL  = -1,
     ZERO_SOL = 0,
-    ONE_SOL = 1,
+    ONE_SOL  = 1,
     TWO_SOL = 2,
     NAR = 3,
 };
@@ -28,23 +32,29 @@ struct Roots                //structure form for solutions
 //1. Compare two double numbers
 int compare(double x, double y);
 
-//2. Function of enter one double parametr (with security of dumb)
-double enter_d(void);
+//2. Function of checking for overflow numbers
+int check(double z);
 
 //3. Function of clean input of rubbish
 void clean(void);
 
-//4. Main function of Quadratic solver
-void q_solver(Roots *sols, double *argu);
+//4. Function of enter one double parametr (with security of dumb)
+double enter_d(void);
 
-//5. Function of solving specific Linal equations
-void l_solver(Roots *sols, double *argu);
+//5. Function of enters many arguments
+void input_argu(double *argu);
 
 //6. Function of processing solutions and output
 void process_out(Roots *sols, int counter);
 
-//7. Function of checking for overflow numbers
-int check(double z);
+//7. Main function of Quadratic solver
+void q_solver(Roots *sols, double *argu);
 
-//8. Function of enters many arguments
-void input_argu(double *argu);
+//8. Function of solving specific Linal equations
+void l_solver(Roots *sols, double *argu);
+
+//9. Function of making one test
+int test_solo(int *n, double a, double b, double c, double x1_cor, double x2_cor, int amount_cor);
+
+//10. Function of all tests(many solo)
+void test_all(void);
