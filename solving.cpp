e21b41a68160e12e193ffeb5e848b0e7ea02f03a
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-23 19:47:57 +0300

    Наведена красота и автоматизирована работа

--- solving.cpp
+++ solving.cpp
@@ -3,32 +3,34 @@
 //7. Main function of Quadratic solver and 8. Function of solving specific Linal equations
 
 //7.
-void q_solver(Roots *sols, double *argu)
+void quadratic_solver(Roots *sols, double *args)
 {
     double x1 = NAN;
     double x2 = NAN;
     double d = NAN;
 
-    if(compare(argu[0],0))
+    if (compare (args[0], 0))
     {
-        l_solver(sols,argu);
+        linal_solver (sols, args);
     }
     else
     {
-        d = argu[1] * argu[1] - 4 * argu[0] * argu[2];
-        if(d > 0)
-        {
-            x1 = (- argu[1] + sqrt(d)) / (2 * argu[0]);
-            x2 = (- argu[1] - sqrt(d)) / (2 * argu[0]);
+        d = args[1] * args[1] - 4 * args[0] * args[2];
+        if (d > 0)
+        {   
+            double sqrt_d = sqrt (d);
+            x1 = (- args[1] + sqrt_d) / (2 * args[0]);
+            x2 = (- args[1] - sqrt_d) / (2 * args[0]);
             sols -> amount = 2;
             sols -> root1 = x1;
             sols -> root2 = x2;
         }
-        else if(compare(d,0))
+        else if (compare (d, 0)) 
         {
-            x1 = - argu[1] / (2 * argu[0]);
+            x1 = - args[1] / (2 * args[0]);
             sols -> amount = 1;
-            sols -> root1 = x1;
+            if (compare (x1, 0)) sols -> root1 = fabs (x1);
+            else sols -> root1 = x1; 
         }
         else
         {
@@ -39,12 +41,12 @@ void q_solver(Roots *sols, double *argu)
 }
 
 //8.
-void l_solver(Roots *sols, double *argu)
+void linal_solver(Roots *sols, double *argu)
 {
     double x1 = NAN;
-    if(compare(argu[1],0))
+    if (compare(argu[1], 0))
     {
-        if(compare(argu[2],0))
+        if (compare(argu[2], 0))
         {
             sols -> amount = -1;
         }
@@ -56,8 +58,8 @@ void l_solver(Roots *sols, double *argu)
     else
     {
         sols -> amount = 1;
-        x1 = argu[2] / argu[1];
-        if (compare(x1,0)) sols -> root1 = fabs(-(argu[2] / argu[1]));
-        else sols -> root1 = -(argu[2] / argu[1]);
+        x1 = - argu[2] / argu[1];
+        if (compare(x1, 0)) sols -> root1 = fabs (x1);
+        else sols -> root1 = x1;
     }
 }
