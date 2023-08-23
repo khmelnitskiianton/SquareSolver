6ded3e6c511aa721385739abcf463cb52383cdf7
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-23 12:32:03 +0300

    Добавлено: массив для коэфф, общий ввод, красота

--- allfunc.h
+++ allfunc.h
@@ -2,6 +2,7 @@
 //constants with define and enum
 
 #define EPSILONE 1e-36      //invite epsilone near to 0 for comparing
+#define QUANTITY 3          //quantity of parametrs (3 in quadratic equation)
 
 enum solutions{
     INF_SOL = -1,
@@ -14,7 +15,7 @@ enum solutions{
 //--------------------------------------------------------------------------------------
 //Structure for solutions
 
-struct Roots          //structure form for solutions
+struct Roots                //structure form for solutions
 {
     int amount;
     double root1;
@@ -28,19 +29,22 @@ struct Roots          //structure form for solutions
 int compare(double x, double y);
 
 //2. Function of enter one double parametr (with security of dumb)
-double enter(void);
+double enter_d(void);
 
 //3. Function of clean input of rubbish
 void clean(void);
 
-//4. Main function of square solver
-void qua_solver(Roots *sols, double a, double b, double c);
+//4. Main function of Quadratic solver
+void q_solver(Roots *sols, double *argu);
 
-//5. Function of solving specific linal equations
-void lin_solver(Roots *sols, double b, double c);
+//5. Function of solving specific Linal equations
+void l_solver(Roots *sols, double *argu);
 
 //6. Function of processing solutions and output
-void process(Roots *sols, int counter);
+void process_out(Roots *sols, int counter);
 
 //7. Function of checking for overflow numbers
 int check(double z);
+
+//8. Function of enters many arguments
+void input_argu(double *argu);
