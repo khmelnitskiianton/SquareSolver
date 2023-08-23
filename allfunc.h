7749c554e188bfaa874c0350f8747948ddbb60e2
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-23 19:47:31 +0300

    Наведена красота и автоматизирована работа

--- allfunc.h
+++ allfunc.h
@@ -1,19 +1,20 @@
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
+#include <ctype.h>
 
 //--------------------------------------------------------------------------------------
 //constants with define and enum
 
-#define EPSILONE 1e-36      //invite epsilone near to 0 for comparing
-#define QUANTITY 3          //quantity of parametrs (3 in quadratic equation)
+const double EPSILONE = 1e-36;      //invite epsilone near to 0 for comparing
+const int QUANTITY = 3;          //quantity of parametrs (3 in quadratic equation)
 
-enum solutions{
+enum solutions {
     INF_SOL  = -1,
     ZERO_SOL = 0,
     ONE_SOL  = 1,
-    TWO_SOL = 2,
-    NAR = 3,
+    TWO_SOL  = 2,
+    NAR      = 3,
 };
 
 //--------------------------------------------------------------------------------------
@@ -30,31 +31,37 @@ struct Roots                //structure form for solutions
 //Initialization all Functions
 
 //1. Compare two double numbers
-int compare(double x, double y);
+int compare (double x, double y);
 
 //2. Function of checking for overflow numbers
-int check(double z);
+int check_double (double z);
 
 //3. Function of clean input of rubbish
-void clean(void);
+void clean_buffer (void);
 
 //4. Function of enter one double parametr (with security of dumb)
-double enter_d(void);
+double enter_double (void);
 
 //5. Function of enters many arguments
-void input_argu(double *argu);
+void input_args (double *args);
 
 //6. Function of processing solutions and output
-void process_out(Roots *sols, int counter);
+void process_out (Roots *sols, int counter);
 
 //7. Main function of Quadratic solver
-void q_solver(Roots *sols, double *argu);
+void quadratic_solver (Roots *sols, double *args);
 
 //8. Function of solving specific Linal equations
-void l_solver(Roots *sols, double *argu);
+void linal_solver (Roots *sols, double *args);
 
 //9. Function of making one test
-int test_solo(int *n, double a, double b, double c, double x1_cor, double x2_cor, int amount_cor);
+int test_solo (int *n, double a, double b, double c, double x1_cor, double x2_cor, int amount_cor);
 
-//10. Function of all tests(many solo)
-void test_all(void);
+//10. Function of all tests (many solo)
+int test_all (void);
+
+//11. Function of connection
+int welcome (void);
+
+//12. Function for filler of NAN
+void filler_coeffs(double *args);
