59b8e0c837b763ce3884c54ee4900f92c8f6e2ea
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-24 18:42:00 +0300

    Мелочи и удобство замены цветов подстановкой

--- file_check.cpp
+++ file_check.cpp
@@ -5,8 +5,8 @@ FILE * openfile (void)
     FILE *file_testing = NULL;
     if ((file_testing = fopen ("tests.txt", "r")) == NULL) //check for opening
     {
-        printf ("\033[1;31m!!! ERROR OF OPENING FILE !!!\033[1;0m\n");
-        assert(0);
+        printf ("\033[1;%dm!!! ERROR OF OPENING FILE !!!\033[1;%dm\n", RED, RESET);
+        MYASSERT(0);
     }
-    else return file_testing;
+    return file_testing;
 }
