a5bcb17c7c7c633b35db3e77996cff8c965afc88
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-24 18:43:45 +0300

    Мелочи и удобство замены цветов подстановкой

--- out.cpp
+++ out.cpp
@@ -5,22 +5,22 @@ void process_out (Roots *sols, int counter)
     switch (counter)
     {
         case INF_SOL:
-            printf ("\033[1;32mInfinity of solutions. a = b = c = 0\033[1;0m");
+            printf ("\033[1;%dmInfinity of solutions. a = b = c = 0\033[1;%dm", GREEN, RESET);
             break;
 
         case ZERO_SOL:
-            printf ("\033[1;32mNo solutions.\033[1;0m");
+            printf ("\033[1;%dmNo solutions.\033[1;%dm", GREEN, RESET);
             break;
 
         case ONE_SOL:
-            printf ("\033[1;32mOne solution. x = %lf\033[1;0m", sols -> root1);
+            printf ("\033[1;%dmOne solution. x = %lf\033[1;%dm", GREEN, sols -> root1, RESET);
             break;
 
         case TWO_SOL:
-            printf ("\033[1;32mTwo solutions.\nx1 = %lf\nx2 = %lf\033[1;0m", (*sols).root1, (*sols).root2);
+            printf ("\033[1;%dmTwo solutions.\nx1 = %lf\nx2 = %lf\033[1;%dm", GREEN, (*sols).root1, (*sols).root2, RESET);
             break;
 
         default:
-            printf ("\033[1;31mERROR PROBLEMS IN LOGIC. YOUR LOGIC. CHECK UR BRAINS. AMOUNT NOT NORMAL\033[1;0m");
+            printf ("\033[1;%dmERROR PROBLEMS IN LOGIC. YOUR LOGIC. CHECK UR BRAINS. AMOUNT NOT NORMAL\033[1;%dm",RED, RESET);
     }
 }
