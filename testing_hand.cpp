75d81c329a0f2d89ebd3336b99d272a26149cdbb
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-24 17:48:36 +0300

    Добавлена расцветка вывода и myassert в main

--- testing_hand.cpp
+++ testing_hand.cpp
@@ -6,7 +6,7 @@
 int test_solo(int *n, double a, double b, double c, double x1_cor, double x2_cor, int amount_cor)
 {
     struct Roots decisions = {                  // initialization struct and array
-        NAR,
+        UNDEFINED_SOL,
         NAN,
         NAN,
     };
@@ -16,13 +16,13 @@ int test_solo(int *n, double a, double b, double c, double x1_cor, double x2_cor
 
     if (!(compare (decisions.root1, x1_cor) || compare (decisions.root1, x2_cor)) || !(compare (decisions.root2, x2_cor) || compare (decisions.root2, x1_cor)) || !compare (decisions.amount, amount_cor) )
     {
-        printf("TEST %d FAILED: x1 = %lf , x2 = %lf , amount = %d."
+        printf("\033[1;31mTEST %d FAILED: x1 = %lf , x2 = %lf , amount = %d."
         "Expected: x1_cor = %lf , x2_cor = %lf , amount_cor = "
-        "%d\n", *n, decisions.root1, decisions.root2, decisions.amount, x1_cor, x2_cor, amount_cor);
+        "%d\033[1;0m\n", *n, decisions.root1, decisions.root2, decisions.amount, x1_cor, x2_cor, amount_cor);
     }
     else
     {
-        printf ("TEST %d OK\n", *n);
+        printf ("\033[1;32mTEST %d OK\033[1;0m\n", *n);
     }
     return 1;
 }
