a3b6661ab4d205a32473bb1f7cfaad293f8662be
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-24 17:45:31 +0300

    Добавлена расцветка вывода и myassert в main

--- main.cpp
+++ main.cpp
@@ -4,34 +4,34 @@ int main()
 {   
     double coeffs[QUANTITY] = {};                   // array of coeffs
 
-    filler_coeffs(coeffs);                          //initialization of coeffs
+    filler_coeffs (coeffs);                          //initialization of coeffs
 
     struct Roots solutions = {                      // initialization struct for form of solutions
-        NAR,
+        UNDEFINED_SOL,
         NAN,
         NAN,
     };
 
-    int end = welcome();                            //all connection in one fuction
+    int end = welcome ();                            //all connection in one fuction
     if (end) return 0;
 
-    input_args(coeffs);                             //enter all parametrs with security
+    input_args (coeffs);                             //enter all parametrs with security
 
-    assert(check_double(coeffs[0]));                //Checking for errors
-    assert(check_double(coeffs[1]));
-    assert(check_double(coeffs[2]));
+    MYASSERT(check_double (coeffs[0]));                //Checking for errors
+    MYASSERT(check_double (coeffs[1]));
+    MYASSERT(check_double (coeffs[2]));
     
     //3. Processing quadratic solver
 
-    quadratic_solver(&solutions,coeffs);            //call quadratic solver
+    quadratic_solver(&solutions, coeffs);            //call quadratic solver
 
     int nSolutions = solutions.amount;              // initialization amount of solutions
 
-    assert(check_double(solutions.amount));         //Checking for errors
-    assert(check_double(solutions.root1));
-    assert(check_double(solutions.root2));
+    MYASSERT(check_double (solutions.amount));         //Checking for errors
+    MYASSERT(check_double (solutions.root1));
+    MYASSERT(check_double (solutions.root2));
 
-    process_out(&solutions, nSolutions);            //call function of output
+    process_out (&solutions, nSolutions);            //call function of output
 
     return 0;
 }
