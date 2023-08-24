8ec5123a93c0f6e41f54059bb25a2d5c6f284fa3
ANTON <142332024+khmelnitskiianton@users.noreply.github.com>
2023-08-24 17:43:28 +0300

    Добавлена расцветка вывода и myassert в main

--- file_check.cpp
+++ file_check.cpp
@@ -1,11 +1,11 @@
 #include "allfunc.h"
 
-FILE * openfile(void)
+FILE * openfile (void)
 {
     FILE *file_testing = NULL;
     if ((file_testing = fopen ("tests.txt", "r")) == NULL) //check for opening
     {
-        printf ("!!! ERROR OF OPENING FILE !!!\n");
+        printf ("\033[1;31m!!! ERROR OF OPENING FILE !!!\033[1;0m\n");
         assert(0);
     }
     else return file_testing;
